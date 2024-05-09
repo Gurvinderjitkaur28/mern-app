e3dfd93fdd9ec66087fef83c5286baa91a87a1a2
Gurvinderjitkaur28 <159113408+GurvinderjitKaur@users.noreply.github.com>
2024-05-09 14:23:29 +0530

    changes in user.js

--- routes/user.js
+++ routes/user.js
@@ -12,17 +12,7 @@ router.get("/", handleGetAllUsers);
 
 router.post("/",handleCreateNewUser);
 
-// app.post('/api/users', (req, res) => {
-//     const newUser = req.body;
-//     user.push({ ...newUser, id: user.length + 1 });
-//     fs.writeFile("./MOCK_DATA.json", JSON.stringify(user), (err, data) => {
-//         if (err) {
-//             console.error("Error writing to file:", err);
-//             return res.status(500).json({ error: "Internal server error" });
-//         }
-//         return res.json({ status: "success", id: user.length });
-//     });
-// });
+
 
 router.route("/:id").get(handleGetUserById)
 
